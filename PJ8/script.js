5f7e2954a8a64a0a39ee03fe5118d7e60a4ef33c
vietpham153 <vietpham111102@gmail.com>
2025-01-18 12:38:20 +0700

    counter number

---
+++ PJ8/script.js
@@ -0,0 +1,20 @@
+let count =0;
+
+const value = document.querySelector("#counter-number");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(btn =>{
+    btn.addEventListener("click", e=>{
+        let elementId = e.currentTarget.id;
+
+        if(elementId == 'decrease'){
+            count--;
+        }else if(elementId == 'reset'){
+            count =0;
+        }else{
+            count++;
+        }
+    })
+    value.textContent = count;
+    value.style.color = count > 0 ? "green" : count < 0 ? "red" : "#007bff"
+})
