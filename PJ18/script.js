65e1b9f50ada25d6484f2527e8fbb972b3970e89
vietpham153 <vietpham111102@gmail.com>
2025-01-30 22:52:53 +0700

    random box color

---
+++ PJ18/script.js
@@ -0,0 +1,26 @@
+const generateBtn = document.getElementById("generteBtn")
+
+generateBtn.addEventListener("click", updateColor)
+
+function getRandomColor(){
+    const letters = "0123456789ABCDEF"
+    let color = "#";
+
+    for(let i=0;i<6;i++){
+        color += letters[Math.floor(Math.random() * 16)];
+        console.log(color);
+        
+    }
+    return color;
+}
+
+
+function updateColor(){
+    const colorText = document.getElementById("colorText")
+    const colorBox = document.getElementById("colorBox");
+
+    const randomColor = getRandomColor();
+
+    colorBox.style.backgroundColor = randomColor;
+    colorText.textContent = randomColor;
+}
