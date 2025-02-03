de683f6bfa659e3d3ee3a889ed599ee9cee647de
vietpham153 <vietpham111102@gmail.com>
2025-02-03 21:47:48 +0700

    PJ19

---
+++ PJ19/script.js
@@ -0,0 +1,10 @@
+let arr = [1,2,3,4,5];
+for(let value of arr){
+    console.log(value);
+}
+
+let obj = { a: 5, b: 6, c: 3}
+for(let object in obj){
+    console.log(object);
+    console.log(obj[object]);
+}
