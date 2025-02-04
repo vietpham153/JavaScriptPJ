8d839e58dcf9b14fea28e8952704bbf98599f184
vietpham153 <vietpham111102@gmail.com>
2025-02-04 23:00:47 +0700

    test

--- PJ19/script.js
+++ PJ19/script.js
@@ -1,10 +1,11 @@
-let arr = [1,2,3,4,5];
-for(let value of arr){
-    console.log(value);
-}
+const num1 = 5;
+const num2 = 3;
 
-let obj = { a: 5, b: 6, c: 3}
-for(let object in obj){
-    console.log(object);
-    console.log(obj[object]);
-}
+console.log(num1 & num2);
+console.log(num1 | num2);
+console.log(num1 ^ num2);
+console.log(~num1);
+console.log(num1 << num2);
+console.log(num1 >> num2);
+console.log(~num2);
+
